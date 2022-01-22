ebeadc8be4776eaca4063d0c305b43a53fd6b112
aogu <aogu89goo415@gmail.com>
2022-01-22 17:03:39 +0900

    second commit

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+
+const Header = ({ title }) => {
+  return (
+  <header className='header'>
+      <h1 >{title}</h1>
+      <button className='btn'>Add</button>
+  </header>
+  )
+}
+
+Header.defaultProps = {
+    title : 'Task Tracker',
+} 
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// CSS in jus
+// const headingStyle = {
+//     color: 'red', backgroundColor: 'black'
+// }
+  
+
+export default Header
