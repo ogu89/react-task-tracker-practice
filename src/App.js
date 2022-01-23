e83c131a8546cc47715afdcbe0f53079cf3fd36d
aogu <aogu89goo415@gmail.com>
2022-01-23 16:24:24 +0900

    add dave task btn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
 
 const App = () =>{   
   const [tasks, setTasks] = useState([
@@ -24,7 +26,14 @@ const App = () =>{
    },
   ]) 
   
-  
+  // Add Task
+
+  const addTask = (task) =>{
+    const id = Math.floor(Math.random() * 10000) + 1
+    
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
 
 
   //Delete Task
@@ -41,6 +50,7 @@ const App = () =>{
   return ( 
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ?  <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No tasks to show' }
     </div>
   );
